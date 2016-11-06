0ac1f472dc65863e4503299e498b2645db1e1f22
Cindy Gao <1zxg@queensu.ca>
2016-11-06 08:49:36 -0500

    got greyscale version of etch and sketch working

--- scripts.js
+++ scripts.js
@@ -3,10 +3,6 @@ var num = 16;
 
 $(document).ready(function(){
 
-      var grid = document.createElement("table");
-	  grid.setAttribute("id","grid");
-	  document.body.appendChild(grid);
-
 	  generateFixedGrid();
 
 	  $("td").hover(function(){
@@ -16,7 +12,7 @@ $(document).ready(function(){
 });
 
 function generateFixedGrid() {
-	/*var grid = document.createElement("table");
+	var grid = document.createElement("table");
     grid.setAttribute("id","grid");
 
 
@@ -30,7 +26,7 @@ function generateFixedGrid() {
     container.setAttribute("id","grid");
 
     document.body.appendChild(container);*/
-    //document.body.appendChild(grid);
+    document.body.appendChild(grid);
 
 
 
@@ -54,11 +50,28 @@ function generateFixedGrid() {
 function clearGrid(){
 
 	cells = document.getElementsByTagName('td');
-	/*console.log(cells);*/
 	for (var i=0; i<cells.length; i++){
-    	/* each item means it is a new row, therefore create a new row*/
+    	/* change the colour of each cell in the grid*/
     	cells[i].style.backgroundColor = "white";
     }
+}
+
+function greyScaleGrid(){
+	clearGrid();
+	var cellOpacity = 0;
+
+	cells = document.getElementsByTagName('td');
+	for (var i=0; i<cells.length; i++){
+    	/* change the colour of each cell in the grid*/
+    	cells[i].style.opacity = cellOpacity;
+    }
+
+	$("td").hover(function(){
+	  	$(this).css("opacity", parseFloat($(this).css("opacity")) + 0.2);
+	  });
+
+
+
 }
 
 
@@ -72,25 +85,7 @@ function generateGrid() {
     /*create table, folowed by each row */
     $("#grid").remove();
 
-    var grid = document.createElement("table");
-    grid.setAttribute("id","grid");
-    document.body.appendChild(grid);
-
-
-
-    /*creates mew rows based on given numerical inputs*/
-    for (var i=0; i<num; i++){
-    	/* each item means it is a new row, therefore create a new row*/
-    	var row = grid.insertRow(i);
-    	for (var j=0; j<num; j++){
-    		row.insertCell(j);
-    	}
-    }
-
-    /* this function allows the cells of the table to change colour when you hover over the cell with your mouse*/
-    $("td").hover(function(){
-	      $(this).css("background-color", "black");
-	  });
+    generateFixedGrid();
 
 };
     
